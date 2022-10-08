09e39ce163c7aa3e28a497669adf35db9981bbe4
MaksimKhoruzhenko <114007431+8elar8oy@users.noreply.github.com>
2022-10-08 18:36:13 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	//1
+	/*setlocale(LC_ALL, "rus");
+	int hp;
+	cin >> hp;
+	bool res;
+	res = hp;
+	cout << boolalpha << res << endl;*/
+	//2
+	/*int temp;
+	cin >> temp;
+	if (temp > 20) {
+		cout << "on" << endl;
+	}
+	else if (temp <= 20) {
+		cout << "off" << endl;
+	}*/
+	//3
+	/*int  temppolsovat, tempvkomnate, vlag;
+	cin >> temppolsovat >> tempvkomnate >> vlag;
+	if (tempvkomnate > temppolsovat && vlag <= 80) {
+		cout << "on" << endl;
+	}
+	else if(tempvkomnate <= temppolsovat || vlag>80) {
+		cout << "off" << endl;
+	}*/
+	//4
+	/*double x,y;
+	cin >> x;
+	if (x > 0) {
+		y = sqrt(x - 2);
+		cout << y << endl;
+	}
+	else if (x == 0) {
+		y = 0;
+		cout << y << endl;
+	}
+	else if (x < 0) {
+		y = abs(x);
+		cout << y << endl;
+	}*/
+	//5
+	/*int pal;
+	cin >> pal;
+	switch (pal) {
+	case 1: cout << "Большой палец" << endl; break;
+	case 2:	cout << "Указательный палец" << endl; break;
+	case 3: cout << "Средний палец" << endl; break;
+	case 4: cout << "Безымянный палец" << endl; break;
+	case 5: cout << "Мезинец" << endl; break;
+	default: cout << "Ошибка" << endl; break;
+	}*/
+	//6
+	/*int tempvkomnate, tempnaulice;
+	cin >> tempvkomnate >> tempnaulice;
+	if (tempvkomnate > 20 && tempvkomnate<tempnaulice) {
+		cout << "ac on" << endl;
+	}
+	else if (tempvkomnate<=20 && tempvkomnate < tempnaulice) {
+		cout << "ac off" << endl;
+	}
+
+	else if (tempvkomnate <= 20 && tempnaulice<tempvkomnate) {
+		cout << "open window" << endl;
+	}*/
+
+
+
+}
